511668cb478c7661d2109d8013cf436696e565cb
William Schneider <william.lima.schneider@gmail.com>
2022-09-08 18:23:07 -0300

    first commit

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,6 +2,14 @@
 int main()
 {
 #pragma omp parallel
-    printf(" Hello World from OpenMP \n ");
+    printf("\nSome dois numeros:  ");
+    printf("\nPrimeiro numero: ");
+    int x;
+    scanf("%d", &x);
+    printf("\nSegundo numero: ");
+    int y;
+    scanf("%d", &y);
+    int total = x + y;
+    printf("\nA soma de %d e %d é: %d!\n", x, y, total);
     return 0;
 }
